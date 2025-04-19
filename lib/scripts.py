62b8e5c7cc8ab06ca0840d93f7ad431095f25d08
LennardKrause <48315771+LennardKrause@users.noreply.github.com>
2025-04-19 15:49:18 +0200

    Update and rename scripts.py to scripts.py

--- lib/scripts.py
+++ lib/scripts.py
@@ -1,17 +1,17 @@
 import sys
 from PyQt6 import QtGui, QtWidgets
 import h5py, hdf5plugin
-import BioMAX2sfrm
-from BioMAX2sfrm.classes import ConversionWindow
-from BioMAX2sfrm.functions import prepare_conversion, convert_to_sfrm
+import convert2sfrm
+from convert2sfrm.classes import ConversionWindow
+from convert2sfrm.functions import prepare_conversion, convert_to_sfrm
 
-def run():
+def BioMAX2sfrm():
     app = QtWidgets.QApplication(sys.argv)
     font = QtGui.QFont()
     font.setStyleHint(QtGui.QFont.StyleHint.Monospace)
     app.setFont(font)
     app.setStyle('fusion')
-    win = ConversionWindow(title=f'Convert MicroMAX / BioMAX to .sfrm, version {BioMAX2sfrm.__version__} (released {BioMAX2sfrm.__date__})',
+    win = ConversionWindow(title=f'Convert MicroMAX / BioMAX to .sfrm, version {convert2sfrm.__version__} (released {convert2sfrm.__date__})',
                            fn_suffix='_master.h5',
                            header_ext=['Run', 'Kappa'],
                            # 'add': add to counter
